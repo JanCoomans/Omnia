038f1cc722e437a0462d0004b6f2e0976a39e043
friscit <jan.coomans@student.pxl.be>
2021-05-04 09:15:21 +0200

    Header is gefixed. plusminus

--- Omnia/js/index.js
+++ Omnia/js/index.js
@@ -1,6 +1,12 @@
-function ToggleSearchbar() {
-    let item = document.getElementById("header-search-input");
-    item.style.opacity = ( item.style.opacity ===  "0" ? "1" : "0");
+window.addEventListener("load", handleLoad);
 
+
+function handleLoad() {// Zet alle events hierin, en maak apparte functies.
+    document.getElementById("header-search-button").addEventListener("click", toggleSearchBar);
+}
+
+function toggleSearchBar() {
+    let item = document.getElementById("header-search-input");
+    item.style.display = ( item.style.display ===  "unset" ? "none" : "unset");
 }
 
