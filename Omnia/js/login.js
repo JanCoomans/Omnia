4589e85f30170a46db7029e16b5919dfed5793e3
NickSoons <55700514+NickSoons@users.noreply.github.com>
2021-05-10 15:45:23 +0200

    verder

---
+++ Omnia/js/login.js
@@ -0,0 +1,23 @@
+window.addEventListener("load", handleLoad);
+
+
+function handleLoad() {// Zet alle events hierin, en maak apparte functies.
+    document.getElementById("header-search-input").style.display = "none";
+    document.getElementById("header-search-button").addEventListener("click", toggleSearchBar);
+}
+
+function toggleSearchBar() {
+    let item = document.getElementById("header-search-input");
+    if(item.style.display === "none"){
+        item.style.display = "unset";
+    }
+    else{
+        redirect()
+    }
+
+    function redirect(){
+        let input = document.getElementById("header-search-input").value;
+        document.location.href = "./" + input + ".html";
+    }
+
+}
