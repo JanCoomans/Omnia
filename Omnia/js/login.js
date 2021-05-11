9448bd28f893f1b41e97557f9053795f8f8d4363
NickSoons <55700514+NickSoons@users.noreply.github.com>
2021-05-11 16:15:35 +0200

    update login.js

--- Omnia/js/login.js
+++ Omnia/js/login.js
@@ -2,6 +2,14 @@ let klanten = [
     {naam:'Soons',voornaam:'Nick',email:'Nick.Soons@omnia.com', password:'OMNIA123'}
 ];
 
+document.getElementById("login").addEventListener("click", controleAccount);
+function controleAccount(){
+    let gebruikersnaam = document.getElementById("emailAdress").valueOf();
+    let password = document.getElementById("password").valueOf();
+        console.log(gebruikersnaam + " " + password);
+}
+
+
 document.getElementById("header-search-input").style.display = "none";
 document.getElementById("header-search-button").addEventListener("click", toggleSearchBar);
 
