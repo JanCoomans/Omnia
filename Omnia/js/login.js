7df523cb1716a3453b5fb11c923d09895a82fb61
NickSoons <55700514+NickSoons@users.noreply.github.com>
2021-05-18 10:30:32 +0200

    javascript^

--- Omnia/js/login.js
+++ Omnia/js/login.js
@@ -1,12 +1,19 @@
 let klanten = [
-    {naam:'Soons',voornaam:'Nick',email:'Nick.Soons@omnia.com', password:'OMNIA123'}
+    {naam:'Soons',voornaam:'Nick',email:'Nick.Soons@omnia.com', password:'OMNIA123'},
+    {naam: "Jansens", voornaam: "Jorne", email: ""}
 ];
 
 document.getElementById("login").addEventListener("click", controleAccount);
 function controleAccount(){
     let gebruikersnaam = document.getElementById("emailAdress").valueOf();
     let password = document.getElementById("password").valueOf();
-    alert("U bent ingelogd");
+
+    let gebruikeersNaamValue = gebruikersnaam.value();
+    let passwordvaalue = password.value();
+
+    alert(gebruikeersNaamValue + " " + passwordvaalue);
+    // console.log(gebruikersnaam + " " + password);
+
 }
 
 
