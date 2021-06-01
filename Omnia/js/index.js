d809e5e41fe0892ef5134db59d25f4cd34f67b89
Jan Coomans <jan.coomans@student.pxl.be>
2021-06-01 10:09:57 +0200

    color check

--- Omnia/js/index.js
+++ Omnia/js/index.js
@@ -1,19 +1,1 @@
-document.getElementById("header-search-input").style.display = "none";
-document.getElementById("header-search-button").addEventListener("click", toggleSearchBar);
-
-
-function toggleSearchBar() {
-    let item = document.getElementById("header-search-input");
-    if(item.style.display === "none"){
-        item.style.display = "unset";
-    }
-    else{
-        redirect()
-    }
-
-    function redirect(){
-        let input = document.getElementById("header-search-input").value;
-        document.location.href = "./" + input + ".html";
-    }
-}
 
